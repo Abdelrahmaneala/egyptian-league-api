9186332b016798bb6b1b537b235cb89dabdb7d34
Abdelrahmaneala <139005150+Abdelrahmaneala@users.noreply.github.com>
2025-08-28 12:40:08 +0300

    Update User.js

--- egyptian-league-api/models/User.js
+++ egyptian-league-api/models/User.js
@@ -1,30 +1,9 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6
-  }
-});
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
 module.exports = mongoose.model("User", userSchema);
