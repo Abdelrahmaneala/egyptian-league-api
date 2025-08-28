5b473c16da00f512db9fefa6f9325996bdb8a183
Abdelrahmaneala <139005150+Abdelrahmaneala@users.noreply.github.com>
2025-08-28 12:41:23 +0300

    Update authController.js

--- egyptian-league-api/controllers/authController.js
+++ egyptian-league-api/controllers/authController.js
@@ -1,1 +1,33 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+exports.signup = async (req, res) => {
+  try {
+    const { username, password, role } = req.body;
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const user = new User({ username, password: hashedPass, role });
+    await user.save();
+
+    res.json({ status: "success", data: { username: user.username, role: user.role } });
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user) return res.status(400).json({ status: "fail", message: "User not found" });
+
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) return res.status(400).json({ status: "fail", message: "Invalid password" });
+
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.json({ status: "success", token });
+};
