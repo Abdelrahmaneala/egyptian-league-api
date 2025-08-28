1362a1813472cefcbace12d81ef7d50667bf55cf
Abdelrahmaneala <139005150+Abdelrahmaneala@users.noreply.github.com>
2025-08-28 12:04:39 +0300

    Update team.js

--- egyptian-league-api/models/team.js
+++ egyptian-league-api/models/team.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
 
 const teamSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  city: { type: String, required: true },
-  stadium: { type: String, required: true },
-  foundedYear: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  stadium: {
+    type: String,
+    required: true
+  },
+  logo: {
+    type: String,
+    default: "" // رابط اللوجو
+  }
 });
 
 module.exports = mongoose.model("Team", teamSchema);
