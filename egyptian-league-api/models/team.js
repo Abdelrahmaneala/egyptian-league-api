a5c36bec626e49ee333bc45e09d3fc55a6dd359f
Abdelrahmaneala <139005150+Abdelrahmaneala@users.noreply.github.com>
2025-08-29 22:59:40 +0300

    Delete egyptian-league-api directory

--- egyptian-league-api/models/team.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const teamSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  city: { type: String, required: true },
-  logo: { type: String },
-});
-
-module.exports = mongoose.model("Team", teamSchema);
--- egyptian-league-api/routes/matchRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createMatch, getMatches } = require("../controllers/matchController");
-const { protect, restrictTo } = require("../middleware/authMiddleware");
-
-router.post("/", protect, restrictTo("admin"), createMatch);
-router.get("/", getMatches);
-
-module.exports = router;
