eb4d52b2c64fbeb8b372e76ea0087903102f7a3a
Abdelrahmaneala <139005150+Abdelrahmaneala@users.noreply.github.com>
2025-08-29 23:15:15 +0300

    Update Team.js

--- models/Team.js
+++ models/Team.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true },
   city: { type: String, required: true },
-  logo: { type: String }, // ✅ مضاف عشان التاسك الأخير
+  logo: { type: String },
 });
 
 module.exports = mongoose.model("Team", teamSchema);
