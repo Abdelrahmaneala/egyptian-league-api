a5f1db1fc95f93c75e4547b5f72128cd3c48a77d
Abdelrahmaneala <139005150+Abdelrahmaneala@users.noreply.github.com>
2025-08-29 23:11:22 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,21 +2,17 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-// Register User
 exports.registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // Check if user exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Save user
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
 
@@ -29,20 +25,16 @@ exports.registerUser = async (req, res) => {
   }
 };
 
-// Login User
 exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check if user exists
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
-    // Compare password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
-    // Create token
     const token = jwt.sign(
       { id: user._id, email: user.email },
       process.env.JWT_SECRET,
